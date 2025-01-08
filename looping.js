03c4c53278c4e9709361500d405ddf1f2608b9e0
sreeja2618 <2211cs010571@mallareddyuniversity.ac.in>
2025-01-08 10:26:27 +0530

    pushing data

---
+++ looping.js
@@ -0,0 +1,56 @@
+//for-loop
+for(let i=1;i<=5;i++){
+    console.log("Sreeja");
+}
+let n1=100;
+for(let i=1;i<=n1;i++){
+    console.log("Sreeja");
+}
+let n=100;
+for(let i=1;i<=n;i++){
+    if(i%2==0){
+        console.log(i+" is an even number.");
+    }
+    else{
+        console.log(i+" is an odd number.");
+    }
+}
+
+//while-loop
+let a=10;
+while(a<=20){
+    console.log("Sreeja");
+    a++;
+}
+
+//do-while
+let i=1;
+do{
+    console.log("Sreeja");
+    i++
+}while(i<=10)
+
+// let i1=1;
+// do{
+//     console.log("Sreeja");
+//     i1++
+// }while(i>=10)
+
+for(let i=1;i<=10;i++){
+    console.log(i);
+    if(i==5)
+        break;
+}
+
+for(let i=1;i<=10;i++){
+    if(i==5)
+        continue;
+    console.log(i);
+}
+function addtocart(){
+    alert("Product is added to cart");
+}
+function add(a,b){
+    console.log(a+b);
+}
+add(10,40);
