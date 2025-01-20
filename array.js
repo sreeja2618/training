2de0bb6ef099d035572014d5b2aa268342aaac15
sreeja2618 <2211cs010571@mallareddyuniversity.ac.in>
2025-01-20 12:56:29 +0530

    pushing

--- array.js
+++ array.js
@@ -3,7 +3,7 @@
 // const u3="ss";
 // const u4="ll";
 // const u5="oo";
-// const users=["ss","dd","ss","ll","oo"];
+const users=["ss","dd","ss","ll","oo"];
 // console.log(users[0]);
 // console.log(users[1]);
 // console.log(users[2]);
@@ -14,15 +14,16 @@
 // }
 
 //map returns one more array
-// let x=users.map((user)=>{
-//     return (user);
-// })
-// console.log(x);
-// users.forEach((user)=>{
-
-// })
+let x=users.map((user)=>{
+    return (user);
+})
+console.log(x);
+let y=users.forEach((user)=>{
+return(user)
+})
+console.log(y);
 //methods in array
-const users=["ss","dd","ss","ll","oo"];
+// const users=["ss","dd","ss","ll","oo"];
 // users.push("UU");
 // for(let i=0;i<users.length;i++){
 //          console.log(users[i]);
@@ -53,35 +54,47 @@ const users=["ss","dd","ss","ll","oo"];
 // let users2=users.concat(users1)
 // console.log(users2);
 
-let user={
-    name:"sreeja",
-    age:25,
-    city:"adb",
-    address:{
-        area:"ranisathiji",
-        street:{
-        pincode:500401
-        }
+// let user={
+//     name:"sreeja",
+//     age:25,
+//     city:"adb",
+//     address:{
+//         area:"ranisathiji",
+//         street:{
+//         pincode:500401
+//         }
         
-    }
-}
-console.log(user);
-console.log(user.name);
-console.log(user.address)
-console.log(user.address.street.pincode)
+//     }
+// }
+// console.log(user);
+// console.log(user.name);
+// console.log(user.address)
+// console.log(user.address.street.pincode)
 
-//create array of objects
-let array1=[{
-    name:"sreeja",
-    place:"adb"
-},{
-    name:"sreeja",
-    place:"adb"
-},
-{
-    name:"sreeja",
-    place:"adb"}
-]
-array1.map((user)=>{
-    console.log(user.name)
-})
+// //create array of objects
+// let array1=[{
+//     name:"sreeja",
+//     place:"adb"
+// },{
+//     name:"sreeja",
+//     place:"adb"
+// },
+// {
+//     name:"sreeja",
+//     place:"adb"}
+// ]
+// array1.map((user)=>{
+//     console.log(user.name)
+// })
+
+let user={
+ name:"sreeja",
+        age:25,
+        city:"adb",
+}
+x=JSON.stringify(user);
+console.log("JSON OBJECT: "+x);
+
+y=JSON.parse(x);
+console.log(y);
+
