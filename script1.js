7ca7d6abbb6883e7e7eaf791b7118578119bd53a
sreeja2618 <2211cs010571@mallareddyuniversity.ac.in>
2025-01-30 12:47:44 +0530

    pushing

---
+++ script1.js
@@ -0,0 +1,10 @@
+export const college="mru";
+const marks=100;
+
+export function tasking(){
+    console.log("I am leanring code noew at "+college)
+    document.getElementById("hello").innerText="I am leanring code noew at "+college;
+
+}
+export default marks;
+
---
+++ script2.js
@@ -0,0 +1,4 @@
+import {college,tasking} from "./script1.js"
+import total from "./script1.js";
+tasking()
+console.log(total);
