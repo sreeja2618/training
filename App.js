6a91d20bb56063473b8db9ccfc7d6c28119085c1
sreeja2618 <158133695+sreeja2618@users.noreply.github.com>
2025-02-03 12:57:29 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1 style={{color:"red"}}>One hour wasted</h1>
-    </div>
-  );
+import React from "react";
+// function App() {
+//   // let ele=React.createElement("div",{className:"App"},
+//   //   React.createElement("h1",null,"Header")
+//   // )
+//   let username="sreeja";
+//   return (
+//     // ele
+//     // <div className="App Name">
+//     //   <h1>Header</h1>
+//     //   <h2>ffdfd</h2>
+//     //  </div>
+//     // <div>
+//     // <h1>{username}</h1>
+//     // <p>{10+10}</p>
+//     // </div>
+    
+//   );
+// }
+// class App extends React.Component{
+//   render(){
+//     return(
+//       <h1>Class based object</h1>
+//     )
+//   }
+// }
+function App(){
+  return(
+    <h1>Function Based object</h1>
+  )
 }
 
 export default App;
